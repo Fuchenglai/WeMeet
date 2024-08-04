47f4b50e48cdcbd3a6ad1e289d8d8191290dfb4e
Fucheng_lai <fucheng_lai@163.com>
2024-08-04 16:14:27 +0800

    通过自定义View实现拖拽验证码

---
+++ app/src/main/java/com/lfc/wemeet/test/TestActivity.java
@@ -0,0 +1,19 @@
+package com.lfc.wemeet.test;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.lfc.framework.base.BaseActivity;
+
+/**
+ * @Author: LaiFuCheng
+ * @CreateTime: 2024/8/4
+ * @Profile: 测试专用类
+ */
+public class TestActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
